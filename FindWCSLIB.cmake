e6943b8c0631743cd9acffcec7a8dd64bfa1371d
Lars Baehren <lbaehren@gmail.com>
2012-12-03 09:50:01 +0100

    Use find_package_handle_standard_args() function to handle standard arguments for a find module; find module for WCSLIB.

--- FindWCSLIB.cmake
+++ FindWCSLIB.cmake
@@ -18,69 +18,61 @@
 # |   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                 |
 # +-----------------------------------------------------------------------------+
 
-# - Check for the presence of <PACKAGE>
+# - Check for the presence of WCSLIB
 #
-# The following variables are set when <PACKAGE> is found:
-#  <PACKAGE>_FOUND      = Set to true, if all components of <PACKAGE> have been found.
-#  <PACKAGE>_INCLUDES   = Include path for the header files of <PACKAGE>
-#  <PACKAGE>_LIBRARIES  = Link these to use <PACKAGE>
-#  <PACKAGE>_LFLAGS     = Linker flags (optional)
+# The following variables are set when WCSLIB is found:
+#  WCSLIB_FOUND      = Set to true, if all components of WCSLIB have been found.
+#  WCSLIB_INCLUDES   = Include path for the header files of WCSLIB
+#  WCSLIB_LIBRARIES  = Link these to use WCSLIB
+#  WCSLIB_LFLAGS     = Linker flags (optional)
 
-if (NOT <PACKAGE>_FOUND)
+if (NOT WCSLIB_FOUND)
 
-  if (NOT <PACKAGE>_ROOT_DIR)
-    set (<PACKAGE>_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
-  endif (NOT <PACKAGE>_ROOT_DIR)
+  if (NOT WCSLIB_ROOT_DIR)
+    set (WCSLIB_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
+  endif (NOT WCSLIB_ROOT_DIR)
 
   ##_____________________________________________________________________________
   ## Check for the header files
 
-  find_path (<PACKAGE>_INCLUDES <header file(s)>
-    HINTS ${<PACKAGE>_ROOT_DIR} ${CMAKE_INSTALL_PREFIX}
-    PATH_SUFFIXES include
+  find_path (WCSLIB_INCLUDES wcs.h
+    HINTS ${WCSLIB_ROOT_DIR} ${CMAKE_INSTALL_PREFIX}
+    PATH_SUFFIXES include include/wcs include/wcslib include/wcslib-4.4.4
     )
 
   ##_____________________________________________________________________________
   ## Check for the library
 
-  find_library (<PACKAGE>_LIBRARIES <package name>
-    HINTS ${<PACKAGE>_ROOT_DIR} ${CMAKE_INSTALL_PREFIX}
+  find_library (WCSLIB_LIBRARIES wcs
+    HINTS ${WCSLIB_ROOT_DIR} ${CMAKE_INSTALL_PREFIX}
     PATH_SUFFIXES lib
-    )
-
-  ##_____________________________________________________________________________
-  ## Check for the executable
-
-  find_program (<PACKAGE>_EXECUTABLE <package name>
-    HINTS ${<PACKAGE>_ROOT_DIR} ${CMAKE_INSTALL_PREFIX}
-    PATH_SUFFIXES bin
     )
 
   ##_____________________________________________________________________________
   ## Actions taken when all components have been found
 
-  find_package_handle_standard_args (<PACKAGE> DEFAULT_MSG <PACKAGE>_LIBRARIES <PACKAGE>_INCLUDES)
+  find_package_handle_standard_args (WCSLIB DEFAULT_MSG WCSLIB_LIBRARIES WCSLIB_INCLUDES)
 
-  if (<PACKAGE>_FOUND)
-    if (NOT <PACKAGE>_FIND_QUIETLY)
-      message (STATUS "Found components for <PACKAGE>")
-      message (STATUS "<PACKAGE>_ROOT_DIR  = ${<PACKAGE>_ROOT_DIR}")
-      message (STATUS "<PACKAGE>_INCLUDES  = ${<PACKAGE>_INCLUDES}")
-      message (STATUS "<PACKAGE>_LIBRARIES = ${<PACKAGE>_LIBRARIES}")
-    endif (NOT <PACKAGE>_FIND_QUIETLY)
-  else (<PACKAGE>_FOUND)
-    if (<PACKAGE>_FIND_REQUIRED)
-      message (FATAL_ERROR "Could not find <PACKAGE>!")
-    endif (<PACKAGE>_FIND_REQUIRED)
-  endif (<PACKAGE>_FOUND)
+  if (WCSLIB_FOUND)
+    if (NOT WCSLIB_FIND_QUIETLY)
+      message (STATUS "Found components for WCSLIB")
+      message (STATUS "WCSLIB_ROOT_DIR  = ${WCSLIB_ROOT_DIR}")
+      message (STATUS "WCSLIB_INCLUDES  = ${WCSLIB_INCLUDES}")
+      message (STATUS "WCSLIB_LIBRARIES = ${WCSLIB_LIBRARIES}")
+    endif (NOT WCSLIB_FIND_QUIETLY)
+  else (WCSLIB_FOUND)
+    if (WCSLIB_FIND_REQUIRED)
+      message (FATAL_ERROR "Could not find WCSLIB!")
+    endif (WCSLIB_FIND_REQUIRED)
+  endif (WCSLIB_FOUND)
 
   ##_____________________________________________________________________________
   ## Mark advanced variables
 
   mark_as_advanced (
-    <PACKAGE>_ROOT_DIR
-    <PACKAGE>_INCLUDES
-    <PACKAGE>_LIBRARIES
+    WCSLIB_ROOT_DIR
+    WCSLIB_INCLUDES
+    WCSLIB_LIBRARIES
     )
 
-endif (NOT <PACKAGE>_FOUND)
+endif (NOT WCSLIB_FOUND)
